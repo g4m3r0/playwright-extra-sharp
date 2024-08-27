70c552dd3dbcc6d0869e2ef9e69c62f58b975d76
Lucas Schmutzler <9322695+g4m3r0@users.noreply.github.com>
2024-08-27 19:03:53 +0200

    feat: Add UnitTesting project

---
+++ PlaywrightExtraSharp.Tests/PlaywrightExtraSharpTests.cs
@@ -0,0 +1,37 @@
+﻿using PlaywrightExtraSharp.Models;
+
+namespace PlaywrightExtraSharp.Tests;
+
+using System.Threading.Tasks;
+using Xunit;
+using PlaywrightExtraSharp;
+using PlaywrightExtraSharp.Plugins.ExtraStealth;
+
+public class PlaywrightExtraSharpTests
+{
+    [Fact]
+    public async Task GoToAsyncHeadless_WhenNavigatingToGoogleHomePage_ShouldDisplayGoogleTitle()
+    {
+        // Initialize PlaywrightExtra with Chromium
+        var playwrightExtra = await new PlaywrightExtra(BrowserTypeEnum.Chromium)
+            .Install()
+            .Use(new StealthExtraPlugin())
+            .LaunchAsync(new ()
+            {
+                Headless = true
+            });
+
+        // Create a new page
+        var page = await playwrightExtra.NewPageAsync(null);
+
+        // Navigate to Google
+        await page.GotoAsync("http://google.com");
+
+        // Assert that the page title is "Google"
+        var title = await page.TitleAsync();
+        Assert.Equal("Google", title);
+
+        // Close the browser
+        await playwrightExtra.CloseAsync();
+    }
+}
